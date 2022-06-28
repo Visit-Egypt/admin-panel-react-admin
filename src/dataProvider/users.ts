631ca065f7d8800682d4e06dfff0510141597223
Ziad Emad <ziadzizo2125@gmail.com>
2022-06-28 11:29:24 +0200

    update users dataprovider to use async-await

--- src/dataProvider/users.ts
+++ src/dataProvider/users.ts
@@ -1,145 +1,168 @@
 import { stringify } from "query-string";
 import { fetchUtils, DataProvider } from "ra-core";
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { HttpError } from "react-admin";
 import { User, UsersPageResponse } from "../types";
 
 let dataProviderFunctions = {
-  getList(resource: string, params: any, apiUrl: string) {
-    const { page, perPage } = params.pagination;
-    // const { field, order } = params.sort;
-    let userData = JSON.parse(localStorage.getItem("auth") as string);
+  async getList(resource: string, params: any, apiUrl: string) {
+    try {
+      const { page, perPage } = params.pagination;
+      let userData = JSON.parse(localStorage.getItem("auth") as string);
+      let response: AxiosResponse<UsersPageResponse> = await axios.get(
+        `${apiUrl}/api/user/all`,
+        {
+          params: {
+            page_num: page,
+            limit: perPage,
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${userData.token_type} ${userData.access_token}`,
+          },
+        }
+      );
+      console.log(response);
 
-    return axios
-      .get(`${apiUrl}/api/user/all`, {
-        params: {
-          page_num: page,
-          limit: perPage,
-        },
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${userData.token_type} ${userData.access_token}`,
-        },
-      })
-      .then((response: AxiosResponse<UsersPageResponse>) => {
-        console.log(response);
-        return {
-          data: response.data.users,
-          total: response.data.content_range,
-        };
-      })
-      .catch((err) => {
-        return Promise.reject(
-          new HttpError(
-            (err.response.data && err.response.data.errors[0]) || "Error",
-            err.response.data.status_code,
-            err.response.data
-          )
-        );
-      });
+      return {
+        data: response.data.users,
+        total: response.data.content_range,
+      };
+    } catch (error: any) {
+      return Promise.reject(
+        new HttpError(
+          error.response.data?.errors[0] || "Error",
+          error.response.data?.status_code,
+          error.response.data
+        )
+      );
+    }
   },
   async getOne(resource: string, params: any, apiUrl: string) {
     let userData = JSON.parse(localStorage.getItem("auth") as string);
-    return axios
-      .get(`${apiUrl}/api/user`, {
-        params: {
-          user_id: params.id,
-          user_email: params.email,
-        },
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${userData.token_type} ${userData.access_token}`,
-        },
-      })
-      .then((response: AxiosResponse<User>) => {
-        return {
-          data: response.data,
-        };
-      })
-      .catch((err) => {
-        return Promise.reject(
-          new HttpError(
-            (err.response.data && err.response.data.errors[0]) || "Error",
-            err.response.data.status_code,
-            err.response.data
-          )
-        );
-      });
+
+    try {
+      let response: AxiosResponse<UsersPageResponse> = await axios.get(
+        `${apiUrl}/api/user`,
+        {
+          params: {
+            user_id: params.id,
+            user_email: params.email,
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${userData.token_type} ${userData.access_token}`,
+          },
+        }
+      );
+      console.log(response);
+
+      return {
+        data: response.data,
+      };
+    } catch (error: any) {
+      return Promise.reject(
+        new HttpError(
+          error.response.data?.errors[0] || "Error",
+          error.response.data?.status_code,
+          error.response.data
+        )
+      );
+    }
   },
   async getMany(resource: string, params: any, apiUrl: string) {
     let userData = JSON.parse(localStorage.getItem("auth") as string);
 
-    let ResolvedRequests = await Promise.all(
-      params.ids.map((id: string) => {
-        return axios
-          .get(`${apiUrl}/api/user`, {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `${userData.token_type} ${userData.access_token}`,
-            },
-            params: {
-              user_id: id,
-            },
-          })
-          .then((response: AxiosResponse<User>) => {
-            return response.data;
-          });
-      })
-    );
-    return { data: ResolvedRequests };
+    try {
+      let response: AxiosResponse<UsersPageResponse>[] = await Promise.all(
+        params.ids.map((id: string) => {
+          return axios
+            .get(`${apiUrl}/api/user`, {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `${userData.token_type} ${userData.access_token}`,
+              },
+              params: {
+                user_id: id,
+              },
+            })
+            .then((response: AxiosResponse<User>) => {
+              return response.data;
+            });
+        })
+      );
+      console.log(response);
+
+      return { data: response };
+    } catch (error: any) {
+      return Promise.reject(
+        new HttpError(
+          error.response.data?.errors[0] || "Error",
+          error.response.data?.status_code,
+          error.response.data
+        )
+      );
+    }
   },
-  delete(resource: string, params: any, apiUrl: string) {
-    let userData = JSON.parse(localStorage.getItem("auth") as string);
-    return axios
-      .delete(`${apiUrl}/api/user/${params.id}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${userData.token_type} ${userData.access_token}`,
-        },
-      })
-      .then((response: AxiosResponse<User>) => {
-        return {
-          data: response.data,
-        };
-      })
-      .catch((err) => {
-        return Promise.reject(
-          new HttpError(
-            (err.response.data && err.response.data.errors[0]) || "Error",
-            err.response.data.status_code,
-            err.response.data
-          )
-        );
-      });
-  },
-  create(resource: string, params: any, apiUrl: string) {
+  async delete(resource: string, params: any, apiUrl: string) {
     let userData = JSON.parse(localStorage.getItem("auth") as string);
 
-    return axios
-      .post(`${apiUrl}/api/user/register`, params.data, {
-        params: {
-          user_id: params.id,
-        },
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${userData.token_type} ${userData.access_token}`,
-        },
-        data: params.data,
-      })
-      .then((response: AxiosResponse<User>) => {
-        return {
-          data: { ...params.data, ...response.data },
-        };
-      })
-      .catch((err) => {
-        return Promise.reject(
-          new HttpError(
-            (err.response.data && err.response.data.errors[0]) || "Error",
-            err.response.data.status_code,
-            err.response.data
-          )
-        );
-      });
+    try {
+      let response: AxiosResponse<User> = await axios.delete(
+        `${apiUrl}/api/user/${params.id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${userData.token_type} ${userData.access_token}`,
+          },
+        }
+      );
+      console.log(response);
+
+      return {
+        data: response.data,
+      };
+    } catch (error: any) {
+      return Promise.reject(
+        new HttpError(
+          error.response.data?.errors[0] || "Error",
+          error.response.data?.status_code,
+          error.response.data
+        )
+      );
+    }
+  },
+  async create(resource: string, params: any, apiUrl: string) {
+    let userData = JSON.parse(localStorage.getItem("auth") as string);
+
+    try {
+      let response: AxiosResponse<UsersPageResponse> = await axios.post(
+        `${apiUrl}/api/user/register`,
+        params.data,
+        {
+          params: {
+            user_id: params.id,
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${userData.token_type} ${userData.access_token}`,
+          },
+        }
+      );
+      console.log(response);
+
+      return {
+        data: { ...params.data, ...response.data },
+      };
+    } catch (error: any) {
+      return Promise.reject(
+        new HttpError(
+          error.response.data?.errors[0] || "Error",
+          error.response.data?.status_code,
+          error.response.data
+        )
+      );
+    }
   },
   update(resource: string, params: any, apiUrl: string) {
     let userData = JSON.parse(localStorage.getItem("auth") as string);
